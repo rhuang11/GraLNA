187e64dfc6b329d562a17061cd19c2f78ba927ec
Ryan Huang <rhuang1@unc.edu>
2024-06-10 09:41:03 -0400

    added results averaging function

--- results_avg_fn.py
+++ results_avg_fn.py
@@ -6,9 +6,6 @@ df = pd.read_csv('~/GraLNA/RF-FR/results_rf_fr.csv')
 # Filter the DataFrame where the 'topN' column is 0.01
 filtered_df = df[df['topN'] == 0.01]
 
-# Further filter the DataFrame to include only the years between 2003 and 2008
-filtered_df = filtered_df[(filtered_df['Year'] >= 2003) & (filtered_df['Year'] <= 2008)]
-
 # Select the columns of interest
 columns_of_interest = ['auc', 'sensitivity_topk', 'precision_topk', 'ndcg_at_k']
 
