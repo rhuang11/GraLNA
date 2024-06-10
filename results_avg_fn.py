ea0b466ea1417843cec69abec7a8c2e24b52f1b2
Ryan Huang <rhuang1@unc.edu>
2024-06-10 09:48:27 -0400

    added results averaging function

--- results_avg_fn.py
+++ results_avg_fn.py
@@ -1,20 +1,9 @@
-import pandas as pd
+import pandas as pandas
 
-# Load the data into a DataFrame
 df = pd.read_csv('~/GraLNA/RF-FR/results_rf_fr.csv')
 
-# Filter the DataFrame where the 'topN' column is 0.01
-filtered_df = df[df['topN'] == 0.01]
+data = df['metrics']
 
-metrics_list = filtered_df['metrics'].apply(json.loads)
+df2 = pd.DataFrame([data])
 
-# Create a DataFrame from the list of dictionaries
-metrics_df = pd.DataFrame(metrics_list.tolist())
-
-# Calculate the mean for the selected metrics
-averaged_metrics = metrics_df[['auc', 'sensitivity_topk', 'precision_topk', 'ndcg_at_k']].mean()
-
-# Convert the result to a DataFrame with a single row for display purposes
-averaged_df = averaged_metrics.to_frame().T
-
-print(averaged_df)
+print df2
